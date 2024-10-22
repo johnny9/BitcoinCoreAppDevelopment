c7880e0a2f1c6b9abae08a24a726d4f9bd9efd9e
johnny9 <985648+johnny9@users.noreply.github.com>
2024-10-22 15:59:35 -0400

    qml: Define the Units Enum in BitcoinAmount

--- src/qml/bitcoinamount.h
+++ src/qml/bitcoinamount.h
@@ -7,16 +7,37 @@
 
 #include <QObject>
 #include <QString>
+#include <qobjectdefs.h>
 
 class BitcoinAmount : public QObject
 {
+    enum class Unit {
+        BTC,
+        SAT
+    };
+    Q_ENUM(Unit)
+
     Q_OBJECT
+    Q_PROPERTY(Unit unit READ unit WRITE setUnit NOTIFY unitChanged)
+    Q_PROPERTY(QString unitLabel READ unitLabel WRITE setUnitLabel NOTIFY unitLabelChanged)
 
 public:
     explicit BitcoinAmount(QObject *parent = nullptr);
 
+    Unit unit() const;
+    void setUnit(Unit unit);
+
+
 public Q_SLOTS:
     QString sanitize(const QString &text);
+
+Q_SIGNALS:
+    void unitChanged();
+    void unitLabelChanged();
+
+private:
+    Unit m_unit;
+    QString m_unitLabel;
 };
 
 #endif // BITCOIN_QML_BITCOINAMOUNT_H
