52cd2b5c4a556fcbbaa9c1b0c646e236dffab7e3
epicleafies <lukelpollard@gmail.com>
2026-03-18 19:31:35 -0400

    build: Add Qt6::Widgets and optional DBus support for system tray
    QSystemTrayIcon requires QApplication (not QGuiApplication), so switch
    the main binary and the unit test runner to QApplication and add
    Qt6::Widgets to the build. On Linux, Qt may route tray icon ownership
    through the DBus StatusNotifierItem protocol, so add a WITH_DBUS option
    (on by default for non-Windows/macOS/Android targets) that conditionally
    finds and links Qt6::DBus. Wire in the two new unit test translation
    units for the tray models so the test binary picks them up.

--- test/test_unit_tests_main.cpp
+++ test/test_unit_tests_main.cpp
@@ -2,13 +2,15 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <QGuiApplication>
+#include <QApplication>
 
 #include <util/translation.h>
 
 const TranslateFn G_TRANSLATION_FUN{nullptr};
 
 int RunBitcoinAmountTests(int argc, char* argv[]);
+int RunDesktopTrayIconControllerTests(int argc, char* argv[]);
+int RunDesktopWindowBehaviorModelTests(int argc, char* argv[]);
 int RunPeerListModelTests(int argc, char* argv[]);
 int RunPeerStatsUtilTests(int argc, char* argv[]);
 int RunQmlBitcoinUnitsTests(int argc, char* argv[]);
@@ -19,10 +21,12 @@ int RunOptionsModelTests(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     int status = 0;
     status |= RunBitcoinAmountTests(argc, argv);
+    status |= RunDesktopTrayIconControllerTests(argc, argv);
+    status |= RunDesktopWindowBehaviorModelTests(argc, argv);
     status |= RunPeerListModelTests(argc, argv);
     status |= RunPeerStatsUtilTests(argc, argv);
     status |= RunQmlBitcoinUnitsTests(argc, argv);
